eda40bf97ecfd6bb311416e5182d0a56604e8757
Leeya Davis <leeya463@gmail.com>
2021-07-31 17:40:15 -0400

    completed mortgage calculator

---
+++ lesson02_small_programs/mortgage_calculator.rb
@@ -0,0 +1,140 @@
+require 'pry'
+require 'pry-byebug'
+require 'yaml'
+
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
+def messages(messages)
+  MESSAGES[messages]
+end
+
+def prompt(yml_key)
+  message = messages(yml_key)
+  puts "=> #{message}"
+end
+
+def get_name
+  name = ""
+  loop do
+    prompt("name?")
+    name = gets.chomp
+    
+    break unless name.empty?
+    prompt "valid_name"
+  end
+  name
+end
+
+def print_name(usr_name)
+  puts "=> #{messages('greeting')} #{usr_name}."
+end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  /\d/.match(num) && /^\d*\.?\d{0,3}$/.match(num)
+end
+
+# def number?(num)
+#   integer?(num) || float?(num)
+# end
+
+def only_digits?(num)
+  !(/\D/.match?(num))
+end
+
+def valid_loan_amt?(amt)
+  (amt.to_i > 0) && only_digits?(amt)
+end
+
+def get_loan_amount
+  amount = ""
+  prompt('loan_amount?')
+  loop do
+    amount = gets.chomp
+    
+    break if valid_loan_amt?(amount)
+    prompt('invalid_loan_amt')
+  end
+  amount.to_i
+end
+
+def valid_apr?(num)
+  integer?(num) || float?(num)
+end
+
+def get_apr
+  apr = ""
+  prompt('apr?')
+  loop do
+    apr = gets.chomp
+    
+    break if valid_apr?(apr)
+    prompt('invalid_apr')
+  end
+  apr.to_f
+end
+
+def valid_term?(term)
+  integer?(term)
+end
+
+def get_mo_loan_term
+  duration = ""
+  prompt('loan_duration?')
+  loop do
+    duration = gets.chomp
+    
+    break if valid_term?(duration)
+    prompt('invalid_loan_duration')
+  end
+  duration.to_i
+end
+
+def calculate_monthly_interest(apr)
+  ((apr / 100) / 12)
+end
+
+def calculate_mo_pymnt(loan_amt, mo_int, loan_term)
+  mo_pymnt = loan_amt * (mo_int / (1 - (1 + mo_int)**(-loan_term)))
+  # binding.pry
+  mo_pymnt.floor(2)
+end
+
+def calculate_again?
+  answer = ""
+  prompt('calculate_again?')
+  
+  answer = gets.chomp.downcase
+  %w(y yes).include?(answer)
+end
+# system 'clear'
+# prompt('welcome')
+# sleep 1
+# name = get_name
+# print_name(name)
+# sleep 1
+# prompt('intro')
+# sleep 2
+system 'clear'
+
+loop do
+loan_amount = get_loan_amount
+apr = get_apr
+mo_loan_term = get_mo_loan_term
+
+monthly_interest = calculate_monthly_interest(apr)
+monthly_pymnt = calculate_mo_pymnt(loan_amount, monthly_interest, mo_loan_term)
+
+sleep 1
+prompt('interim_message')
+sleep 2
+system 'clear'
+puts format(messages('result'), loan_amount: "$#{loan_amount}", apr: "#{apr}%", mo_loan_term: "#{mo_loan_term}", monthly_pymnt: "$#{monthly_pymnt}")
+sleep 3
+break unless calculate_again?
+end
+
+prompt('thank_you')

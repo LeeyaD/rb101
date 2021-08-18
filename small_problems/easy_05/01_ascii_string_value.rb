17458baa0d2c9184c9244f3d7beb921efac746da
Leeya Davis <leeya463@gmail.com>
2021-08-17 20:48:57 -0400

    further exploration easy5, #1

--- small_problems/easy_05/01_ascii_string_value.rb
+++ small_problems/easy_05/01_ascii_string_value.rb
@@ -22,3 +22,11 @@ puts ascii_value('Four score') == 984
 puts ascii_value('Launch School') == 1251
 puts ascii_value('a') == 97
 puts ascii_value('') == 0
+
+# Further Exploration
+# There is an Integer method such that:
+
+# char.ord.mystery == char
+char.ord.chr == char
+
+# where mystery is our mystery method. Can you determine what method name should be used in place of mystery? What happens if you try this with a longer string instead of a single character?

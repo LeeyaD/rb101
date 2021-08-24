cee30731de42b95c4668b530ea28c55d5f4b2fc8
Leeya Davis <leeya463@gmail.com>
2021-08-24 12:25:49 -0400

    finished ttt assignment

--- lesson06_slightly_larger_programs/ttt_bonus_features.rb
+++ lesson06_slightly_larger_programs/ttt_bonus_features.rb
@@ -1,6 +1,6 @@
-require 'pry'
-require 'pry-byebug'
+require 'yaml'
 
+MESSAGES = YAML.load_file('ttt_messages.yml')
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9],
   [1, 4, 7], [2, 5, 8], [3, 6, 9],
@@ -9,15 +9,37 @@ WINNING_LINES = [
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+VALID_YES = %w(y yes)
+VALID_OPTION = %w(1 2)
 wins = 2
 scoreboard = { "Player" => 0, "Computer" => 0 }
+
+def messages(message)
+  MESSAGES[message]
+end
+
+def clear_screen
+  system 'clear'
+end
+
+def return_to_continue
+  puts ""
+  puts "Press 'enter' to continue"
+  gets
+  clear_screen
+end
+
+def yml_prompt(yml_key)
+  message = messages(yml_key)
+  prompt(message)
+end
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
 def display_board(brd)
-  system 'clear'
+  clear_screen
   puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts ""
   puts " #{brd[1]} | #{brd[2]} | #{brd[3]}"
@@ -73,44 +95,45 @@ def choose_first_player(chooser)
 end
 
 def who_chooses_first_player?
-  system 'clear'
+  clear_screen
   answer = nil
   loop do
-    prompt "Who decides who goes first? ('1' for you or '2' for the Computer)"
-    answer = gets.chomp
-    break if %w(1 2).include?(answer)
-    prompt "That's not a valid choice. '1' or '2' please."
+    yml_prompt 'who_chooses'
+    answer = gets.chomp.strip.downcase
+    break if VALID_OPTION.include?(answer)
+    yml_prompt 'invalid_1_2_choice'
   end
 
   answer == '1' ? 'Player' : 'Computer'
 end
 
 def player_chooses_first_player
-  system 'clear'
+  clear_screen
   answer = nil
 
   loop do
-    prompt "Who should go first? ('1' for you or '2' for the Computer)"
-    answer = gets.chomp
-    break if %w(1 2).include?(answer)
-    prompt "That's not a valid choice. '1' or '2' please."
+    yml_prompt 'who_goes_first'
+    answer = gets.chomp.strip
+    break if VALID_OPTION.include?(answer)
+    yml_prompt 'invalid_1_2_choice'
   end
 
   answer == '1' ? 'Player' : 'Computer'
 end
 
 def computer_chooses_first_player
+  yml_prompt 'computer_choosing'
+  sleep 2
   %w(Player Computer).sample
 end
 
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a free square," \
-    " your options are #{joinor(empty_squares(brd))}:"
-    square = gets.chomp.to_i
+    prompt "Choose a square, your options are #{joinor(empty_squares(brd))}:"
+    square = gets.chomp.strip.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid choice. Enter a value 1-9."
+    yml_prompt 'invalid_square'
   end
 
   brd[square] = PLAYER_MARKER
@@ -139,15 +162,15 @@ def computer_places_piece!(brd)
   attack = detect_strategic_move(brd, COMPUTER_MARKER)
   threat = detect_strategic_move(brd, PLAYER_MARKER)
 
-  if attack
-    square = move(brd, attack)
-  elsif threat
-    square = move(brd, threat)
-  elsif square_5_free?(brd)
-    square = 5
-  else
-    square = empty_squares(brd).sample
-  end
+  square = if attack
+             move(brd, attack)
+           elsif threat
+             move(brd, threat)
+           elsif square_5_free?(brd)
+             5
+           else
+             empty_squares(brd).sample
+           end
 
   brd[square] = COMPUTER_MARKER
 end
@@ -162,11 +185,7 @@ def place_piece!(brd, player)
 end
 
 def alternate_player(player)
-  if player == 'Player'
-    return 'Computer'
-  else
-    'Player'
-  end
+  player == 'Player' ? 'Computer' : 'Player'
 end
 
 def board_full?(brd)
@@ -193,12 +212,13 @@ def update_score(winner, score)
 end
 
 def display_score(score)
+  sleep 1
   puts ""
   prompt "The score is..."
   prompt "Player: #{score['Player']} | Computer: #{score['Computer']}"
 end
 
-def score_sequence(winner, score)
+def update_and_display_score(winner, score)
   update_score(winner, score) if winner
   display_score(score)
 end
@@ -224,37 +244,66 @@ def reset_score(score)
 end
 
 def play_again?
+  sleep 1
   puts ""
-  prompt "Play again? (y/n)"
-  answer = gets.chomp
-  answer.downcase.start_with?('y')
+  yml_prompt 'play_again?'
+  answer = gets.chomp.strip.downcase
+  VALID_YES.include?(answer)
 end
 
+def show_rules?
+  yml_prompt 'show_rules?'
+  answer = gets.chomp.strip
+  VALID_YES.include?(answer)
+end
+
+def show_rules
+  clear_screen
+  yml_prompt 'rules'
+  return_to_continue
+end
+
+def welcome_sequence
+  clear_screen
+  yml_prompt 'welcome'
+  sleep 1
+  return_to_continue
+  show_rules if show_rules?
+end
+
+def display_gameplay_outcome(brd)
+  if someone_won?(brd)
+    winner = detect_winner(brd)
+    prompt "#{winner} won!"
+  else
+    prompt "It's a tie!"
+  end
+  winner
+end
+
+def game_play(brd, current_player)
+  loop do
+    display_board(brd)
+    place_piece!(brd, current_player)
+    current_player = alternate_player(current_player)
+    break if someone_won?(brd) || board_full?(brd)
+  end
+  display_board(brd)
+end
+
+welcome_sequence
 
 loop do
   board = initialize_board
   current_player = who_goes_first_sequence
 
-  loop do
-    display_board(board)
-    place_piece!(board, current_player)
-    current_player = alternate_player(current_player)
-    break if someone_won?(board) || board_full?(board)
-  end
+  game_play(board, current_player)
+  winner = display_gameplay_outcome(board)
 
-  display_board(board)
-
-  if someone_won?(board)
-    winner = detect_winner(board)
-    prompt "#{winner} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  score_sequence(winner, scoreboard)
+  update_and_display_score(winner, scoreboard)
   grand_winner_sequence(scoreboard, wins)
 
   break unless play_again?
 end
 
-prompt "Thanks for playing Tic-Tac-Toe! Goodbye!"
+yml_prompt 'goodbye'

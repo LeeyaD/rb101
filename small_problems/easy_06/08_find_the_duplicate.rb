c845afd76aaee7ede115a167af9b5d1cfeb35f87
Leeya Davis <leeya463@gmail.com>
2021-09-09 09:01:01 -0400

    completed easy6, #8 exercise

--- small_problems/easy_06/08_find_the_duplicate.rb
+++ small_problems/easy_06/08_find_the_duplicate.rb
@@ -22,8 +22,12 @@
 #  - 
 #  2b. IF it appears twice, save & return in a variable
 
-find_dup([1, 5, 3, 1]) == 1
-find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+def find_dup(array)
+  array.select { |n| array.count(n) == 2 }.first
+end
+
+puts find_dup([1, 5, 3, 1]) == 1
+puts find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
           38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
           14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
           78, 76, 48, 16, 93, 77, 52, 49, 37, 29,

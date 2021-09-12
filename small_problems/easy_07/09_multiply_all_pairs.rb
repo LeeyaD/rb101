1ecc16ec5c212faef5ad33a52df0e23bd0ad10d9
Leeya Davis <leeya463@gmail.com>
2021-09-12 17:41:31 -0400

    finished easy_7 exercises

---
+++ small_problems/easy_07/09_multiply_all_pairs.rb
@@ -0,0 +1,14 @@
+# Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of every pair of numbers that can be formed between the elements of the two Arrays. The results should be sorted by increasing value.
+
+# You may assume that neither argument is an empty Array.
+
+def multiply_all_pairs(array1, array2)
+  list = array1.product(array2)
+  sorted_list = list.map do |a| 
+    a.reduce(:*) 
+  end.sort
+
+  sorted_list
+end
+
+p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]

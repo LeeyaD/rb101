d7124a413bd5829ce551dc54004a7ced1a1fa9f2
Leeya Davis <leeya463@gmail.com>
2021-10-28 07:34:04 -0400

    created easy_8 #7 & #8 files

---
+++ small_problems/easy_08/07_double_char_1.rb
@@ -0,0 +1,6 @@
+# Write a method that takes a string, and returns a new string in which every character is doubled.
+
+# Examples:
+repeater('Hello') == "HHeelllloo"
+repeater("Good job!") == "GGoooodd  jjoobb!!"
+repeater('') == ''

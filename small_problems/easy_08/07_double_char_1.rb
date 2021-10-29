223585c12b894a2cb785cad5dd52840d357b51cb
Leeya Davis <leeya463@gmail.com>
2021-10-28 20:17:57 -0400

    uploaded completed small exercises from phone

--- small_problems/easy_08/07_double_char_1.rb
+++ small_problems/easy_08/07_double_char_1.rb
@@ -1,6 +1,29 @@
 # Write a method that takes a string, and returns a new string in which every character is doubled.
 
+#Input: String
+#- empty string returns an empty string
+#Output: new String
+
+# DS: Array
+# ALGO
+# - with input, string
+# - separate into an array of chars
+# - iterate thru array, for each char
+# --- multiply char by 2 and add to a results string
+# return results string
+
+def repeater(string)
+  results = ""
+  str_array = string.chars
+  
+  str_array.each do |char|
+    results << char * 2
+  end
+  
+  results
+end
+
 # Examples:
-repeater('Hello') == "HHeelllloo"
-repeater("Good job!") == "GGoooodd  jjoobb!!"
-repeater('') == ''
+puts repeater('Hello') == "HHeelllloo"
+puts repeater("Good job!") == "GGoooodd  jjoobb!!"
+puts repeater('') == ''
--- small_problems/easy_08/08_double_char_2.rb
+++ small_problems/easy_08/08_double_char_2.rb
@@ -1,8 +1,42 @@
 # Write a method that takes a string, and returns a new string in which every consonant character is doubled. Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
 
+#Input: String
+#Output: new String
+# - only consonants are doubled
+# - all other characters (vowels, digits, punctuation, whitespace) should be ignored
+# - case sensitive (i.e. "S" becomes "SS")
+
+#DS: Array
+#ALGO:
+# with our input, string
+# separate into an array of charscters
+# iterate thru array & for each char:
+# > DETERMINE if char is a consonant
+# >  if yes, double & add to results array
+# >  if no, add to results array as is
+# > return results array
+def consonant?(char)
+  ("A".."z").include?(char) &&
+  !(%w(a e i o u).include?(char))
+end
+
+def double_consonants(string)
+  str_array = string.chars
+  result = ""
+  
+  str_array.each do |char|
+    if consonant?(char)
+      result << char * 2
+    else
+      result << char
+    end
+  end
+  
+  result
+end
 # Examples:
-double_consonants('String') == "SSttrrinngg"
-double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
-double_consonants("July 4th") == "JJullyy 4tthh"
-double_consonants('') == ""
+puts double_consonants('String') == "SSttrrinngg"
+puts double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+puts double_consonants("July 4th") == "JJullyy 4tthh"
+puts double_consonants('') == ""
 

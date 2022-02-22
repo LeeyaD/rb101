c893d1d7dc5388edd16a0b0f012df9d876978f2e
Leeya Davis <leeya463@gmail.com>
2022-02-21 20:26:13 -0500

    Completed Medium 1, #3 exercise

---
+++ small_problems/medium_1/03_rotate_pt3.rb
@@ -0,0 +1,32 @@
+# If you take a number like 735291, and rotate it to the left, you get 352917. If you now keep the first digit fixed in place, and rotate the remaining digits, you get 329175. Keep the first 2 digits fixed in place and rotate again to 321759. Keep the first 3 digits fixed in place and rotate again to get 321597. Finally, keep the first 4 digits fixed in place and rotate the final 2 digits to get 321579. The resulting number is called the maximum rotation of the original number.
+
+# Write a method that takes an integer as argument, and returns the maximum rotation of that argument. You can (and probably should) use the rotate_rightmost_digits method from the previous exercise.
+# Note that you do not have to handle multiple 0s.
+
+def rotate_array(array)
+  array_dup = array.clone
+  array_dup << array_dup.shift
+end
+
+def rotate_rightmost_digits(digits, ro_num)
+  arr_digits = digits.to_s.chars
+  rotating_num = arr_digits.slice!(-(ro_num)..-1)
+  (arr_digits + rotate_array(rotating_num)).join.to_i
+end
+
+def max_rotation(number)
+  counter = number.digits.size
+  loop do
+    number = rotate_rightmost_digits(number, counter)
+    counter -= 1
+    break if counter <= 1
+  end
+
+  number
+end
+
+max_rotation(735291) == 321579
+max_rotation(3) == 3
+max_rotation(35) == 53
+max_rotation(105) == 15 # the leading zero gets dropped
+max_rotation(8_703_529_146) == 7_321_609_845
